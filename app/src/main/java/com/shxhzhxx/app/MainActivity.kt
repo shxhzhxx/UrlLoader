fa653eeea6d4a570b56b0a4c913a98b6fd26d0c2
SH <sh.ylzhxx@163.com>
2019-03-02 20:09:44 +0800

    tmp

--- app/src/main/java/com/shxhzhxx/app/MainActivity.kt
+++ app/src/main/java/com/shxhzhxx/app/MainActivity.kt
@@ -7,12 +7,15 @@ import android.graphics.Rect
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.shxhzhxx.urlloader.UrlLoader
+import com.shxhzhxx.urlloader.TaskManagerEx
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.actor
+import kotlinx.coroutines.sync.Mutex
 import java.io.File
 import java.io.IOException
 import java.util.*
-import java.util.concurrent.*
+import kotlin.coroutines.CoroutineContext
 
 const val TAG = "MainActivity"
 const val URL_BIG = "https://static.usasishu.com/bigFile.pdf"
@@ -20,57 +23,38 @@ const val URL_BIG_VIDEO = "https://static.usasishu.com/bigVideoFile.mp4"
 const val URL_MAX_AGE = "http://plpwobkse.bkt.clouddn.com/ic_launcher.png"
 const val URL_BIG_IMG = "http://plpwobkse.bkt.clouddn.com/1125-2436-72.png"
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Default + job
     private var bitmap: Bitmap? = null
+    lateinit var job: Job
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        job = Job()
         setContentView(R.layout.activity_main)
 
-        val url = URL_BIG
-        val loader = UrlLoader(cacheDir)
-
         download.setOnClickListener {
-            Thread {
-                val task  = FutureTask<String> {work()}
-
-                val threadPool = Executors.newCachedThreadPool()
-                val f = threadPool.submit {
-                    //某同步任务，1s后取消
-                    try {
-                        task.run()
-                    } catch (e: Throwable) {
-                        Log.d(TAG, "run exception:${e.message}")
-                        Log.d(TAG, "isCancelled 1:${task.isCancelled}")
-                        Log.d(TAG, "isDone 1:${task.isDone}")
-                    }
-                    try {
-                        Log.d(TAG, "get 1:${task.get()}")
-                        Log.d(TAG, "future.isCancelled:${task.isCancelled}")
-                    } catch (e: Exception) {
-                        Log.d(TAG, "get 1 exception:${e.message}")
-                        Log.d(TAG, "isCancelled 1:${task.isCancelled}")
-                        Log.d(TAG, "isDone 1:${task.isDone}")
-                    }
-                }
-
-                Thread.sleep(1000)
-                Thread {
-                    //另一个监听者，需要在任务被取消后重启任务
-                    try {
-                        task.run()
-                        Log.d(TAG,"run")
-                        Log.d(TAG, "get 2:${task.get()}")
-                        Log.d(TAG,"get 3:${task.get()}")
-                    } catch (e: Exception) {
-                        Log.d(TAG, "get 2 exception:${e.message}")
-                        Log.d(TAG, "isCancelled 2:${task.isCancelled}")
-                        Log.d(TAG, "isDone 2:${task.isDone}")
-                    }
-                }.start()
-                Thread.sleep(1000)
-                f.cancel(true)
-            }.start()
+            val promise = async(start = CoroutineStart.LAZY) {
+                coroutineWork()
+            }
+            val task = async {
+                Log.d(TAG,"task:${promise.await()}")
+            }
+            launch {
+                Log.d(TAG,"launch:${promise.await()}")
+                delay(2000)
+            }
+            launch {
+                delay(1000)
+                task.cancel()
+            }
         }
+
         cancel.setOnClickListener {
         }
         clear.setOnClickListener {
@@ -78,17 +62,55 @@ class MainActivity : AppCompatActivity() {
         check.setOnClickListener {
         }
     }
+}
 
-    private fun work(): String? {
+suspend fun failedConcurrentSum(): Int = coroutineScope {
+    val one = async<Int> {
+        try {
+            delay(Long.MAX_VALUE) // Emulates very long computation
+            42
+        } finally {
+            println("First child was cancelled")
+        }
+    }
+    val two = async<Int> {
+        delay(1000)
+        println("Second child throws an exception")
+        throw ArithmeticException()
+    }
+    one.await() + two.await()
+}
+
+suspend fun coroutineWork(progress: ((Float) -> Unit)? = null): String? {
+    return runBlocking{
         Log.d(TAG, "work start:${Thread.currentThread().id}")
-
-        Thread.sleep(5000)
+        repeat(10) {
+            try {
+                delay(500)
+            } catch (e: CancellationException) {
+                Log.d(TAG, "CancellationException")
+                return@runBlocking null
+            } finally {
+                Log.d(TAG, "finally")
+            }
+            progress?.invoke(it.toFloat() / 10)
+        }
         val result = UUID.randomUUID().toString()
         Log.d(TAG, "work finish:$result")
-        return result
+        return@runBlocking result
     }
 }
 
+fun work(progress: ((Float) -> Unit)? = null): String? {
+    Log.d(TAG, "work start:${Thread.currentThread().id}")
+    repeat(10) {
+        Thread.sleep(500)
+        progress?.invoke(it.toFloat() / 10)
+    }
+    val result = UUID.randomUUID().toString()
+    Log.d(TAG, "work finish:$result")
+    return result
+}
 
 data class Params(val path: String, val width: Int, val height: Int, val centerCrop: Boolean)
 
@@ -118,4 +140,47 @@ private fun File.decodeBitmap(params: Params): Bitmap? {
         } catch (e: IOException) {
             null
         }
+}
+
+
+class MyCallback(
+        val onComplete: ((String) -> Unit)? = null,
+        val onFailed: (() -> Unit)? = null,
+        val onCanceled: (() -> Unit)? = null,
+        val onProgress: ((progress: Float) -> Unit)? = null
+)
+
+
+class MyTaskManager(maxPoolSize: Int = 10) : TaskManagerEx<MyCallback, String>(maxPoolSize) {
+    fun syncLoad(key: Any, canceled: () -> Boolean): String? {
+        return syncStart(key, { MyTask(key) }, canceled, observer = MyCallback(onProgress = {
+            Log.d(TAG, "syncLoad onProgress:$it")
+        }))
+    }
+
+    fun asyncLoad(key: Any): Int {
+        return asyncStart(key, { MyTask(key) }, observer = MyCallback(onComplete = {
+            Log.d(TAG, "asyncLoad onComplete:$it")
+        }, onProgress = {
+            Log.d(TAG, "asyncLoad onProgress:$it")
+        }, onFailed = {
+            Log.d(TAG, "asyncLoad onFailed")
+        }, onCanceled = {
+            Log.d(TAG, "asyncLoad onCanceled")
+        }))
+    }
+
+    inner class MyTask(key: Any) : Task(key) {
+        override fun doInBackground(): String? {
+            return work { progress ->
+                observers.forEach { it?.onProgress?.invoke(progress) }
+            }.apply {
+                postResult = if (this != null) Runnable {
+                    observers.forEach { it?.onComplete?.invoke(this) }
+                } else Runnable {
+                    observers.forEach { it?.onFailed }
+                }
+            }
+        }
+    }
 }
--- library/src/main/java/com/shxhzhxx/urlloader/TaskManagerEx.kt
+++ library/src/main/java/com/shxhzhxx/urlloader/TaskManagerEx.kt
@@ -29,16 +29,16 @@ open class TaskManagerEx<T, V>(maxPoolSize: Int = CORES) {
 
     /**
      * cancel a task marked by key.
-     * all observer will remain in [Task.observers], and [Task.onObserverUnregistered] won't be invoked for these observers.
+     * all observer will remain in [Task.asyncObservers], and [Task.onObserverUnregistered] won't be invoked for these asyncObservers.
      */
     fun cancel(key: Any) = keyTaskMap[key]?.unregisterAll() != null
 
     /**
      * unregister a observer marked by id,
-     * Contrary to [cancel], removed observer will not remain in [Task.observers],
+     * Contrary to [cancel], removed observer will not remain in [Task.asyncObservers],
      * and [Task.onObserverUnregistered] will been invoked for this observer.
      */
-    fun unregister(id: Int) = idTaskMap[id]?.unregisterObserver(id) != null
+    fun unregister(id: Int) = idTaskMap[id]?.unregisterAsyncObserver(id) != null
 
     fun unregisterByTag(tag: Any) = tagIdsMap[tag]?.toList()?.forEach { id -> unregister(id) } != null
     fun isRunning(key: Any) = keyTaskMap.containsKey(key)
@@ -52,7 +52,7 @@ open class TaskManagerEx<T, V>(maxPoolSize: Int = CORES) {
      * @param observer may be null. Identical instance can be passed multi times.
      * @return non-negative task id. this id may be reused after task is finished or canceled or observer is unregistered.
      */
-    protected fun start(key: Any, builder: () -> Task, tag: Any? = null, observer: T? = null): Int {
+    protected fun asyncStart(key: Any, builder: () -> Task, tag: Any? = null, observer: T? = null): Int {
         val id = kotlin.run {
             var i = 0
             while (idTaskMap.containsKey(i)) {
@@ -61,10 +61,9 @@ open class TaskManagerEx<T, V>(maxPoolSize: Int = CORES) {
             return@run i
         }
         val task = keyTaskMap[key] ?: builder.invoke().also { t ->
-            keyTaskMap[key] = t
-            t.future = threadPoolExecutor.submit(t) as RunnableFuture<V?>
+            t.asyncInit()
         }
-        task.registerObserver(id, observer)
+        task.registerAsyncObserver(id, observer)
         if (tag != null) {
             idTagMap[id] = tag
             val ids = tagIdsMap[tag]
@@ -77,21 +76,14 @@ open class TaskManagerEx<T, V>(maxPoolSize: Int = CORES) {
         return id
     }
 
-    protected fun syncStart(key: Any, builder: () -> Task, tag: Any? = null): V? {
+    protected fun syncStart(key: Any, builder: () -> Task, canceled: () -> Boolean, observer: T? = null): V? {
         val task = synchronized(this) {
             return@synchronized keyTaskMap[key] ?: builder.invoke().also { t ->
-                keyTaskMap[key] = t
-                t.future = FutureTask(t)
+                t.syncInit()
             }
         }
-        try {
-            task.future.run()
-        } catch (e: InterruptedException) {
-            if (!task.isCanceled) {
-                task.future = FutureTask(task)
-            }
-        }
-        return task.future.get()
+        task.registerSyncObserver(observer)
+        return task.syncGet(canceled, observer)
     }
 
     protected fun finalize() {
@@ -119,8 +111,8 @@ open class TaskManagerEx<T, V>(maxPoolSize: Int = CORES) {
          *
          * private set()
          * setter called in two case:
-         * 1.   all observer unregistered ,which [observers] return an empty list;
-         * 2.   [cancel] been called, which [observers] return a list contains current observers.
+         * 1.   all observer unregistered ,which [asyncObservers] return an empty list;
+         * 2.   [cancel] been called, which [asyncObservers] return a list contains current asyncObservers.
          * <p>
          * [onCanceled] will be invoked, avoid running time-consuming tasks in these methods.
          */
@@ -129,15 +121,21 @@ open class TaskManagerEx<T, V>(maxPoolSize: Int = CORES) {
             private set(value) {
                 if (!field && value) {//Cancel operation can only be performed once
                     field = true
-                    future.cancel(true)
+                    asyncFuture?.cancel(true)
+                    syncFuture?.cancel(true)
                     onCanceled()
                 }
             }
 
+        @Volatile
+        var isDone = false
+            private set
+
         /**
          * might contains identical instance multi times.
          */
-        val observers: Iterable<T?> get() = observerMap.map { it.value }
+        val asyncObservers: List<T?> get() = asyncObserverMap.map { it.value }
+        val observers: List<T?> get() = syncObservers.toMutableList().apply { addAll(asyncObservers) }
 
         /**
          * pass whatever you want to doInBackground (in main thread) after [doInBackground] has successfully finished (without cancel nor exception).
@@ -147,13 +145,17 @@ open class TaskManagerEx<T, V>(maxPoolSize: Int = CORES) {
          * in this case, override [onCanceled] instead.
          * <p>
          * the reason that why you should pass result by this method instead of doing it yourself is,
-         * you never know when is safe to return result to observers.
-         * maybe an observer register itself after you return result to other observers and before the task has been cleared.
+         * you never know when is safe to return result to asyncObservers.
+         * maybe an observer register itself after you return result to other asyncObservers and before the task has been cleared.
          * the new comer will die alone without any callback been invoked.
          */
         protected var postResult: Runnable? = null
-        private val observerMap = HashMap<Int, T?>()
-        lateinit var future: RunnableFuture<V?>
+        private val asyncObserverMap = HashMap<Int, T?>()
+        private val syncObservers = HashSet<T?>()
+        @Volatile
+        private var asyncFuture: Future<V?>? = null
+        @Volatile
+        private var syncFuture: FutureTask<V?>? = null
 
         final override fun call(): V? {
             if (isCanceled)
@@ -161,16 +163,62 @@ open class TaskManagerEx<T, V>(maxPoolSize: Int = CORES) {
             return try {
                 val result = doInBackground()
                 runResult(postResult)
+                isDone = true
                 result
-            } catch (e: InterruptedException) {//外部取消
-                if (!isCanceled) {
-
-                }
-                null
+            } catch (e: InterruptedException) {
+                isDone = false
+                throw e
             } catch (e: Throwable) {
                 Log.e(TAG, "Unhandled exception occurs in doInBackground: ${e.message}")
                 runResult(null)
+                isDone = true
                 null
+            }
+        }
+
+        fun syncInit() {
+            keyTaskMap[key] = this
+            syncFuture = FutureTask(this)
+        }
+
+        fun asyncInit() {
+            keyTaskMap[key] = this
+            asyncFuture = threadPoolExecutor.submit(this)
+        }
+
+        fun syncGet(canceled: () -> Boolean, observer: T?): V? {
+            return kotlin.run {
+                while (!canceled.invoke() && !isCanceled) {
+                    syncFuture?.also { future ->
+                        try {
+                            future.run()
+                            return@run future.get()
+                        } catch (e: Throwable) {
+                            if (!canceled.invoke()) {
+                                synchronized(this) {
+                                    if (syncFuture?.isDone == true)
+                                        syncFuture = FutureTask(this)
+                                }
+                            }
+                        }
+                    } ?: return@run try {
+                        asyncFuture?.get()
+                    } catch (e: Throwable) {
+                        null
+                    }
+                }
+                return@run null
+            }.also {
+                unregisterSyncObserver(observer)
+                if (syncObservers.isEmpty()) {
+                    if (asyncObservers.isEmpty()) {
+                        keyTaskMap.remove(key)
+                        isCanceled = true
+                    } else {
+                        if (!isDone)
+                            asyncFuture = threadPoolExecutor.submit(this)
+                    }
+                }
             }
         }
 
@@ -179,7 +227,7 @@ open class TaskManagerEx<T, V>(maxPoolSize: Int = CORES) {
          * in this circumstance, this method will never be invoked.
          */
         @Throws(InterruptedException::class)
-        protected abstract fun doInBackground(): V
+        protected abstract fun doInBackground(): V?
 
         /**
          * invoked when this task is canceled, in main thread.
@@ -189,7 +237,7 @@ open class TaskManagerEx<T, V>(maxPoolSize: Int = CORES) {
 
         /**
          * Note: task which is canceled by [cancel] or [cancelAll],
-         * technically its observers haven't been unregistered, and this func will not been invoked.
+         * technically its asyncObservers haven't been unregistered, and this func will not been invoked.
          *
          * @param observer ob canceled by [unregister] and [unregisterByTag]
          */
@@ -197,11 +245,15 @@ open class TaskManagerEx<T, V>(maxPoolSize: Int = CORES) {
 
         }
 
-        internal fun unregisterObserver(id: Int) {
+        internal fun unregisterSyncObserver(observer: T?) {
+            syncObservers.remove(observer)
+        }
+
+        internal fun unregisterAsyncObserver(id: Int) {
             removeId(id)
-            if (observerMap.containsKey(id)) {
-                val observer = observerMap.remove(id)
-                if (observerMap.isEmpty()) {
+            if (asyncObserverMap.containsKey(id)) {
+                val observer = asyncObserverMap.remove(id)
+                if (observers.isEmpty()) {
                     keyTaskMap.remove(key)
                     isCanceled = true
                 }
@@ -216,11 +268,15 @@ open class TaskManagerEx<T, V>(maxPoolSize: Int = CORES) {
 
         /**
          * @param id       task id
-         * @param observer [observers] will return whatever this param is.
+         * @param observer [asyncObservers] will return whatever this param is.
          */
-        internal fun registerObserver(id: Int, observer: T?) {
-            observerMap[id] = observer
+        internal fun registerAsyncObserver(id: Int, observer: T?) {
+            asyncObserverMap[id] = observer
             idTaskMap[id] = this
+        }
+
+        internal fun registerSyncObserver(observer: T?) {
+            syncObservers.add(observer)
         }
 
         private fun runResult(result: Runnable?) {
@@ -234,11 +290,12 @@ open class TaskManagerEx<T, V>(maxPoolSize: Int = CORES) {
 
         /**
          * remove this task's information from [keyTaskMap] and [idTaskMap]
-         * keep the information of [observerMap] , so that you can return results to observers.
+         * keep the information of [asyncObserverMap] , so that you can return results to asyncObservers.
          */
         private fun clear() {
             keyTaskMap.remove(key)
-            observerMap.map { it.key }.forEach { id -> removeId(id) }
+            asyncObserverMap.map { it.key }.forEach { id -> removeId(id) }
+            syncObservers.clear()
         }
 
     }
